51f0bbbdd2e1c446e977f8f1683db7a5b7c8060a
shaibu13 <samedshaibu55@gmail.com>
2023-05-19 03:20:24 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a funtion that prints a phrase
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
