fca7e9ac0a1a73cbaf986f3f871ff17c7a5e9738
shaibu13 <samedshaibu55@gmail.com>
2023-05-19 21:50:47 +0000

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a function to Print the size of file types
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s\n", sizeof(int));
+	printf("Size of an long int: %d byte(s\n", sizeof(long int));
+	printf("Size of an long long int: %d byte(s\n", sizeof(long long int));
+	printf("Size of an float: %d byte(s\n", sizeof(float));
+
+	return (0);
+}
