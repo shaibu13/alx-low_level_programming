479c97a3ec2eab7e3ccf21178510755c6124b18a
shaibu13 <samedshaibu55@gmail.com>
2023-05-22 22:35:05 +0000

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphhabet backwards
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char tebahpla;
+
+	for (tebahpla = 'z'; tebahpla >= 'a'; tebahpla--)
+
+	putchar(tebahpla);
+	putchar('\n');
+
+		return (0);
+}
